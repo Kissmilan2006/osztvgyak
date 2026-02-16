f426fe6da32b3d72589d32e4015750bc3b07dbfa
Kissmilan2006 <72673076499@rudas.hu>
2026-02-16 12:30:38 +0100

    ljaefh

---
+++ backend/Kiss_Milan_backend/Kiss_Milan_backend/Models/ingatlanok.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Kiss_Milan_backend.Models
+{
+    public class ingatlanok
+    {
+        [Key]
+        public int id { get; set; }
+        public required kategoriak kategoraOBJ { get; set; }
+        [ForeignKey("kategoraOBJ")]
+        public int  kategoria {  get; set; }
+        public string leiras { get; set; }
+        public DateTime hirdetesDatuma { get; set; }
+        public bool tehermentes { get; set; }
+        public int ar { get; set; }
+        public string kepUrl { get; set; }
+    }
+}
---
+++ backend/Kiss_Milan_backend/Kiss_Milan_backend/Models/kategoriak.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Kiss_Milan_backend.Models
+{
+    public class kategoriak
+    {
+       [Key]
+       public int Id { get; set; }
+       public string Name { get; set; } 
+    }
+}
